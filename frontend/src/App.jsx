24909a77ecc3989dab28df5e682fa5a0e9dcec37
digvijay-555 <digvijaydeshmukh5555@outlook.com>
2025-01-03 18:49:24 +0000

    Added the connect Metamask button and other assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import abi from "./assets/abi.json";
+import {ethers} from 'ethers';
+import { use } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [account, setAccount] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const {ethereum} = window;
+
+      if(!ethereum){
+        console.log("Make sure you have MetaMask Installed !");
+        return;
+      }
+      else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts"});
+
+      if (accounts.length!==0){
+        const account = accounts[0];
+        console.log("Found an authorized account", account);
+        setAccount(account);
+      }
+      else{
+        console.log("No authorized account found");
+      }
+
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Connect to MetaMask</h1>
+        {account ? (
+          <p>Connected Account: {account}</p>
+        ) : (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        )}
+    </div>
+      
     </>
   )
 }
